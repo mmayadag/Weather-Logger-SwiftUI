cb530c5b2d8b24b363571d48c83f001f8e36f219
Murat Mayadağ <murat-2@hotmail.com>
2020-03-19 16:52:16 +0300

    :truck: supporting base files

--- WeatherLogger/SupportFiles/SceneDelegate.swift
+++ WeatherLogger/SupportFiles/SceneDelegate.swift
@@ -19,8 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = RootView().environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
