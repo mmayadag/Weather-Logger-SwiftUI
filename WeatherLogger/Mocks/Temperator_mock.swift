fae15a0309fa8b46927bf94d1ad13a4cfaa8910f
Murat Mayadağ <murat-2@hotmail.com>
2020-03-19 16:49:19 +0300

    :clown_face: mock data for testing

---
+++ WeatherLogger/Mocks/Temperator_mock.swift
@@ -0,0 +1,16 @@
+//
+//  Temperator_mock.swift
+//  WeatherLogger
+//
+//  Created by Murat Mayadağ on 16.03.2020.
+//  Copyright © 2020 Murat Mayadağ. All rights reserved.
+//
+
+import Foundation
+
+let temperator_izmir = Main(temp: 17.4, feels_like: 0.91, temp_min: 6.0, temp_max: 8.0, pressure: 1021.0, humidity: 75.0)
+
+let Weather_mock = Weather(id: 1, main: "Clear", description: "Clear", icon: "ids")
+
+let Wind_mock = Wind(speed: 10.5, degree: 10, gust: 15.9)
+
---
+++ WeatherLogger/Mocks/izmir_coordinate_mock.swift
@@ -0,0 +1,11 @@
+//
+//  izmir_coordinate_mock.swift
+//  WeatherLogger
+//
+//  Created by Murat Mayadağ on 16.03.2020.
+//  Copyright © 2020 Murat Mayadağ. All rights reserved.
+//
+
+import Foundation
+
+let izmir_coordinate = Coordinate( lat: 38.449589, lon: 27.181681)
